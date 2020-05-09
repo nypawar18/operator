b346db2797422953cfcf9f8ebb3973a7e9ce7040
nypawar <nypawar@gmail.com>
2020-05-09 16:09:56 +0530

    operator project

---
+++ operator.cpp
@@ -0,0 +1,17 @@
+/*
+ * operator.cpp
+ *
+ *  Created on: 09-May-2020
+ *      Author: nitin
+ */
+#include<iostream>
+using namespace std;
+#include"operator.h"
+int main(void)
+{
+
+	return 1;
+}
+
+
+
---
+++ operator.h
@@ -0,0 +1,15 @@
+/*
+ * operator.h
+ *
+ *  Created on: 09-May-2020
+ *      Author: nitin
+ */
+
+#ifndef OPERATOR_H_
+#define OPERATOR_H_
+
+
+
+
+
+#endif /* OPERATOR_H_ */
